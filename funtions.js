fc2aa0b10bf102c7ab7813d1c454a4fbfb44e385
Jewel Clarke-Henry <YukiTapionCrow@gmail.com>
2019-03-22 02:08:10 +0000

    Update later

--- funtions.js
+++ funtions.js
@@ -1,2 +1,19 @@
 // JavaScript File
-document.write("what's up?");
+$(document).ready(function(){
+ $("#carouselExampleCaptions").carousel({interval: 500});
+ $(".item1").click(function(){
+    $("#carouselExampleCaptions").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#carouselExampleCaptions").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#carouselExampleCaptions").carousel(2);
+  });
+  $(".left").click(function(){
+    $("#carouselExampleCaptions").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#carouselExampleCaptions").carousel("next");
+  });
+});
